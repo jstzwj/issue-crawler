c14b608daa330189019dcf6f8d7cfba8c1cc1d80
jstzwj <1103870790@qq.com>
2019-12-19 14:21:56 +0800

    add new recommend framework

---
+++ recommend.py
@@ -0,0 +1,7 @@
+class RecommendModel(object):
+    def __init__(self):
+        pass
+    def train(self, train_data):
+        pass
+    def recommend(self, user_ids) -> list:
+        pass
--- run_recommend.py
+++ run_recommend.py
@@ -2,8 +2,10 @@
 import datetime
 import numpy
 import tqdm
+import random
 from matplotlib import pyplot as plt 
 from project import Project
+import recommend
 
 def get_user_item_matrix(project, end_time):
     issues = project.get_issues()
@@ -52,61 +54,6 @@ def get_user_item_matrix(project, end_time):
 
     return rate_matrix, users_meta, issues_meta
 
-def PearsonCorrelationSimilarity(vec1, vec2):
-	# value = range(len(vec1))
-	# sum_vec1 = sum([ vec1[i] for i in value])
-	# sum_vec2 = sum([ vec2[i] for i in value])
-    sum_vec1 = numpy.sum(vec1)
-    sum_vec2 = numpy.sum(vec2)
-
-	# square_sum_vec1 = sum([ pow(vec1[i],2) for i in  value])
-	# square_sum_vec2 = sum([ pow(vec2[i],2) for i in  value])
-    square_sum_vec1 = numpy.sum(numpy.power(vec1, 2))
-    square_sum_vec2 = numpy.sum(numpy.power(vec2, 2))
-
-	# product = sum([ vec1[i]*vec2[i] for i in value])
-    product = vec1.dot(vec2)
-
-    numerator = product - (sum_vec1 * sum_vec2 / len(vec1))
-    dominator = ((square_sum_vec1 - pow(sum_vec1, 2) / len(vec1)) * (square_sum_vec2 - pow(sum_vec2, 2) / len(vec2))) ** 0.5
-
-    if dominator == 0:
-        return 0
-    result = numerator / (dominator * 1.0)
-
-    return result
-
-def get_user_similarity(project, rate_matrix, left_index, user_left, right_index, user_right):
-    star_map = {}
-    for each_star in user_left['star']:
-        if each_star not in star_map:
-            star_map[each_star] = len(star_map)
-
-    for each_star in user_right['star']:
-        if each_star not in star_map:
-            star_map[each_star] = len(star_map)
-
-    left_vec = numpy.zeros((len(star_map) + 1))
-    right_vec = numpy.zeros((len(star_map) + 1))
-    
-    for each_star in user_left['star']:
-        left_vec[star_map[each_star]] = 1
-
-    # 保证PearsonCorrelationSimilarity中dominator不为0
-    left_vec[len(star_map)] = 1
-
-    for each_star in user_right['star']:
-        right_vec[star_map[each_star]] = 1
-
-    # 保证PearsonCorrelationSimilarity中dominator不为0
-    right_vec[len(star_map)] = 1
-
-    return numpy.dot(left_vec, right_vec)/(numpy.linalg.norm(left_vec)*numpy.linalg.norm(right_vec))
-
-
-def get_issue_similarity(project, rate_matrix,user_left, user_right):
-    pass
-
 '''
 get close or open state of an issue until sometime 
 '''
@@ -151,47 +98,6 @@ def rec_issues(project, end_time):
 
     user_item_matrix_origin, users_meta_origin, issues_meta_origin = get_user_item_matrix(project, None)
 
-    user_simil_matrix = numpy.zeros((len(users), len(users)))
-
-    with tqdm.tqdm(total=len(users)*len(users)//2) as pbar:
-        for i, each_user_i in enumerate(users):
-            for j, each_user_j in enumerate(users):
-                if j > i:
-                    continue
-                if i == j:
-                    user_simil_matrix[i, j] = 1
-                    continue
-                tmp = get_user_similarity(project, user_item_matrix, i, each_user_i, j, each_user_j)
-                user_simil_matrix[i, j] = tmp
-                user_simil_matrix[j, i] = tmp
-                pbar.update(1)
-
-    prediction_matrix = user_simil_matrix @ user_item_matrix
-    '''
-    prediction_matrix = numpy.zeros((len(users), len(issues)))
-
-    # top k
-    with tqdm.tqdm(total=len(users)) as pbar:
-        user_item_mean = numpy.mean(user_item_matrix, axis=1)
-        user_item_mean_minus = user_item_matrix - numpy.repeat(user_item_mean[:, numpy.newaxis], user_item_matrix.shape[1], axis=1)
-
-        for a in range(len(users)):
-            avg_ru = numpy.mean(user_item_matrix[a, :])
-            k = 1 / (numpy.sum(user_simil_matrix[a, :]) + 0.00001)
-            
-            prediction_matrix[a, :] = avg_ru
-            for u in range(len(users)):
-                prediction_matrix[a, :] += k * (user_simil_matrix[a, u] * user_item_mean_minus[a, :])
-
-            pbar.update(1)
-
-    '''
-
-    # changes from origin
-    change_matrix = (user_item_matrix_origin - user_item_matrix) > 0
-    # print(change_matrix)
-
-
     # filter and score
     '''
     What we need to do here is filtering valid items and users. If an issue had not been shown before, it shall not be used for simulated recommend.
@@ -214,7 +120,7 @@ def rec_issues(project, end_time):
         for each_issue_index in valid_issue:
             if user_item_matrix_origin[each_user_index, each_issue_index] > 0 and \
                 issue_state_before[each_user_index] == 'open':
-                # user_item_matrix[each_user_index, each_issue_index] <= 0:
+                # user_item_matrix[each_user_index, each_issue_index] <= 0 and \
                 # len(issues) - numpy.sum(user_item_matrix[each_user_index, :] == 0) > 3:
                 valid_user.append(each_user_index)
                 break
@@ -231,7 +137,7 @@ def rec_issues(project, end_time):
             for each_issue_index in valid_issue:
                 record.append(
                     (each_issue_index,
-                    prediction_matrix[each_user_index, each_issue_index]
+                    random.random()
                     )
                 )
 
@@ -249,7 +155,70 @@ def rec_issues(project, end_time):
         return correct_counter
 
     # for each_top_k in range(0, 40):
-    return predict(10)/len(valid_user), predict(50)/len(valid_user), predict(100)/len(valid_user)
+    return predict(3)/len(valid_user), predict(5)/len(valid_user), predict(10)/len(valid_user)
+
+def extract_recommend_dataset(project, end_time=None):
+    ret = []
+    issues = project.get_issues()
+    users = project.get_users()
+
+    issues_meta = ({}, {})
+    users_meta = ({}, {})
+
+    for i, each_issue in enumerate(issues):
+        issues_meta[0][i] = each_issue['id']
+        issues_meta[1][each_issue['id']] = i
+
+    for i, each_user in enumerate(users):
+        users_meta[0][i] = each_user['user_name']
+        users_meta[1][each_user['user_name']] = i
+
+    rate_matrix = numpy.zeros((len(users), len(issues)))
+
+    # reorder activities by time
+    for i, each_issue in enumerate(issues):
+        if len(each_issue) <= 0:
+            continue
+        
+        counter = {}
+        for each_timeline in each_issue['timeline']:
+            if each_timeline == {}:
+                continue
+            if 'time' not in each_timeline.keys():
+                continue
+            
+            if end_time is not None and datetime.datetime.strptime(each_timeline['time'], '%Y-%m-%dT%H:%M:%S') > end_time:
+                continue
+
+            if 'author' not in each_timeline.keys():
+                continue
+            
+            if each_timeline['author'] in counter.keys():
+                counter[each_timeline['author']] += 1
+            else:
+                counter[each_timeline['author']] = 1
+        for each_user, each_count in counter.items():
+            if each_user is not None:
+                ret.append((users_meta[1][each_user], i, each_count/len(each_issue['timeline'])))
+
+    return ret
+
+def dataset_split(dataset):
+    length = len(dataset)
+    split_pos = int(length*0.6)
+    random.shuffle(dataset)
+    return dataset[:split_pos], dataset[split_pos:]
+
+class RandomRecommendModel(recommend.RecommendModel):
+    def __init__(self, project):
+        self.issues = project.get_issues()
+        self.users = project.get_users()
+    def train(self, train_data):
+        pass
+    def recommend(self, user_id, k):
+        candidate_issues = list(range(len(self.issues)))
+        random.shuffle(candidate_issues)
+        return candidate_issues[:k]
 
 if __name__ == "__main__":
     project = Project()
@@ -257,36 +226,28 @@ if __name__ == "__main__":
     # project.load('./data/deno', '/denoland/deno')
     project.load('./data/FreeRDP', '/FreeRDP/FreeRDP')
     
-    time_list = [
-        '2019-1-10T05:44:42Z',
-        '2019-2-10T05:44:42Z',
-        '2019-3-10T05:44:42Z',
-        '2019-4-10T05:44:42Z',
-        '2019-5-10T05:44:42Z',
-        '2019-6-10T05:44:42Z',
-        '2019-7-10T05:44:42Z',
-        '2019-8-10T05:44:42Z',
-        '2019-9-10T05:44:42Z',
-        '2019-10-10T05:44:42Z',
-        '2019-11-10T05:44:42Z',
-    ]
-    x_list = list(range(11))
-    top_3_list = []
-    top_5_list = []
-    top_10_list = []
-    for each_time in time_list:
-        ret = rec_issues(project, datetime.datetime.strptime(each_time, '%Y-%m-%dT%H:%M:%SZ'))
-        top_3_list.append(ret[0])
-        top_5_list.append(ret[1])
-        top_10_list.append(ret[2])
+    dataset = extract_recommend_dataset(project)
+    train_data, test_data = dataset_split(dataset)
+    model = RandomRecommendModel(project)
+    model.train(train_data)
 
-    plt.title("Matplotlib demo") 
-    plt.xlabel("x axis caption")
-    plt.ylabel("y axis caption")
-    plt.plot(x_list,top_3_list,"b")
-    plt.plot(x_list,top_5_list,"r") 
-    plt.plot(x_list,top_10_list,"y")
-    plt.show()
-    
+    # test data acceleration structure
+    users = {}
+    for each_data in test_data:
+        if each_data[0] in users.keys():
+            users[each_data[0]].append(each_data)
+        else:
+            users[each_data[0]] = []
 
-    
+    # valid
+    counter = 0
+    for each_user, data_list in users.items():
+        recommend_result = model.recommend(each_user, 3)
+        for user_id, item_id, rate in data_list:
+            if item_id in recommend_result:
+                counter+=1
+                break
+
+    print(f'precision: {counter}/{len(users)}, {counter/len(users)}')
+
+
